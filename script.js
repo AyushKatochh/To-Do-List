38a550c07fbf3a3d2d79568a9804acda00cb72c1
Ayush Katoch <katochayushhp@gmail.com>
2022-12-20 22:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const items = JSON.parse(localStorage.getItem('items')) || [];//
 
 function addItem(e) {
   e.preventDefault();
